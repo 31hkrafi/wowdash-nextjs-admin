db25c6f59aadf5d93a79a9714a9a5e9873837e31
helakKhan2017 <helakKhan2017@users.noreply.github.com>
2026-02-09 09:21:44 +0600

    Add all missing files - hooks, types, utils, configs

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/purity": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "react/no-unescaped-entities": "off",
+      "prefer-const": "off",
+      "@next/next/no-img-element": "off",
+    },
+  },
+];
+
+export default eslintConfig;
